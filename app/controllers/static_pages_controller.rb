8a4e7ab49e5249e8e5c552bee9b6f5697f3126e1
Julien Puchol <julien.puchol@insa-lyon.Fr>
2013-11-14 16:05:56 +0100

    Add StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def blog
+  end
+
+  def help
+  end
+
+  def api
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 BillSplit::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/about"
+  get "static_pages/blog"
+  get "static_pages/help"
+  get "static_pages/api"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
