51d0d8469e16ae914ab1470bcdf80249d4d71290
matjrh-3 <matjrh-3@student.ltu.se>
2013-11-17 22:57:52 +0100

    dd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,15 +27,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
+#    respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @user }
+        flash[:success] = "Welcome to the Bill split!"
+        redirect_to @user
+#        format.html { redirect_to @user, notice: 'User was successfully created.' }
+#        format.json { render action: 'show', status: :created, location: @user }
       else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        render 'new'
+#        format.html { render action: 'new' }
+#        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
-    end
+#    end
   end
 
   # PATCH/PUT /users/1
